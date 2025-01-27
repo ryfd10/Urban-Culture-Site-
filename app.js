21eb7b620fc687aac4878b46c1383a977ff32b0d
תכנות <@690156GUR.LOCAL>
2025-01-27 17:43:09 +0200

    test server

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express =require('express')
-
+const path=require('path')
 const app=express()
 
+
+app.use('/my-style',express.static(path.join(__dirname, './pages/styles')))
+app.use('/my-script',express.static(path.join(__dirname, './pages/scripts')))
 app.get('/',(req,res)=>{
-    res.status(200).send('server')
+    res.status(200).sendFile(path.join(__dirname, './pages/index.html'))
 })
 app.get('/*',(req,res)=>{
     res.status(404).send(`the ${req.url} was not found`)
