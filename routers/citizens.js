0627b9fcdaaca0ddfde2116b8776b72717752264
תכנות <@690156GUR.LOCAL>
2025-02-03 17:17:18 +0200

    get quest from

--- routers/citizens.js
+++ routers/citizens.js
@@ -3,11 +3,20 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/all', (req, res) => {
+    console.log(req.query);
+    const { skip = 0, count = 78 } = req.query
+    console.log({ skip, count});
     res.status(200).json([])
 })
-router.post('/add-family',(req,res)=>{
-    const family=req.body
-    res.status(201).json({...family})
+router.get('/all-street/:streetname/area/:a-name', (req, res) => {
+    console.log(req.params);
+    res.status(200).json([])
+})
+router.post('/add-family', express.json(), (req, res) => {
+    const family = req.body
+    console.table({ family })
+
+    res.status(201).json({ ...family })
 })
 
 
